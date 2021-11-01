b4ef66b6f1e8321f67b7d2ebefba1be3a419c9e8
DouglasV4 <90541167+DouglasV4@users.noreply.github.com>
2021-10-31 19:43:45 -0600

    Add files via upload

---
+++ Crud 1y2/CRUD1Y2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simpleCRUD
+{
+    public partial class Citaa : Form
+    {
+        public Citaa()
+        {
+            InitializeComponent();
+        }
+
+        private void bookToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            REGISTRO form = new REGISTRO();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
